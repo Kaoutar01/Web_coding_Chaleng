dead21743ef0333671ab37ec98c5ab51d140dd15
Kaoutar BELLA <kaoutar.bella02@gmail.com>
2019-07-05 21:44:41 +0100

    scratch Api project

---
+++ ShopsChalengApi/ShopsChalengApi/src/main/java/com/Shops/ShopsChalengApi/ShopsChalengApiApplication.java
@@ -0,0 +1,13 @@
+package com.Shops.ShopsChalengApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsChalengApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsChalengApiApplication.class, args);
+	}
+
+}
---
+++ ShopsChalengApi/ShopsChalengApi/src/test/java/com/Shops/ShopsChalengApi/ShopsChalengApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Shops.ShopsChalengApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopsChalengApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
